62ebc052a492165bf0331ffcfcb50782b6ee438b
Unknown <eduardo.fimbres21@gmail.com>
2018-02-25 02:51:26 -0700

    5

--- CambiandoDestinos/public/js/javascript.js
+++ CambiandoDestinos/public/js/javascript.js
@@ -1,1 +1,11 @@
-
+$('.carousel').carousel({
+  interval: 2500
+})
+function myFunction() {
+    var x = document.getElementById("nave");
+    if (x.className === "navbar") {
+        x.className += " responsive";
+    } else {
+        x.className = "navbar";
+    }
+}
--- CambiandoDestinos/resources/views/index.blade.php
+++ CambiandoDestinos/resources/views/index.blade.php
@@ -11,36 +11,75 @@
 
 <body>
 
-	<nav class="navbar navbar-default navbar-fixed-top" >
+	<nav class="navbar navbar-default navbar-fixed-top" id="nave">
 
 		<div class="container-fluid">
     <div class="navbar-header" >
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+				<span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
       <p class="navbar-brand" >
         <img  src="./img/fclogo1.png" class="img-responsive imgp" alt="Responsive image" >
       </p>
-			<div class="col-md-10 col-md-offset container-fluid contene">
-				<div class="col-md-3 col-md-offset container-fluid contenedor">
-			<a class="col-md-12 col-md-offset" href="#">Protección al Abuelo</a>
-			</div>
-			<div class="col-md-3 col-md-offset container-fluid contenedor">
-			<a class="col-md-12 col-md-offset" href="#">Comida Caliente</a>
-			</div>
-			<div class="col-md-3 col-md-offset container-fluid contenedor">
-			<a class="col-md-12 col-md-offset" href="#">Hoy por Hoy</a>
-			</div>
-			<div class="col-md-3 col-md-offset container-fluid contenedor">
-			<a class="col-md-12 col-md-offset" href="#">Hoy por Hoy</a>
-			</div>
-		</div>
-    </div>
+			<a class="active" href="#">Protección al Abuelo</a>
+			<a  href="#">Comida Caliente</a>
+			<a  href="#">Hoy por Hoy</a>
+			<a  href="#">Hoy por Hoy</a>
+
   </div>
 
 </nav>
 
-	<section id="s1">
+<section id="s1">
 		<div class="col-md-12 col-md-offset landingpage1">
+			<div id="myCarousel" class="carousel slide" data-ride="carousel">
+			<ol class="carousel-indicators">
+	<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+	<li data-target="#myCarousel" data-slide-to="1"></li>
+	<li data-target="#myCarousel" data-slide-to="2"></li>
+</ol>
 
+<!-- Wrapper for slides -->
+<div class="carousel-inner">
+	<div class="item active">
+		<img class="img-responsive imgitem " src="./img/guitarra.JPG" alt="Responsive image">
+		<div class="carousel-caption">
+			<h3>Los Angeles</h3>
+			<p>LA is always so much fun!</p>
 		</div>
+	</div>
+
+	<div class="item">
+		<img class="img-responsive imgitem " src="./img/viejita.jpg"   alt="Responsive image">
+		<div class="carousel-caption">
+			<h3>Chicago</h3>
+			<p>Thank you, Chicago!</p>
+		</div>
+	</div>
+
+	<div class="item">
+		<img class="img-responsive imgitem " src="./img/junta.JPG"  alt="Responsive image">
+		<div class="carousel-caption">
+			<h3>New York</h3>
+			<p>We love the Big Apple!</p>
+		</div>
+	</div>
+</div>
+
+<!-- Left and right controls -->
+<a class="left carousel-control" href="#myCarousel" data-slide="prev">
+	<span class="glyphicon glyphicon-chevron-left"></span>
+	<span class="sr-only">Previous</span>
+</a>
+<a class="right carousel-control" href="#myCarousel" data-slide="next">
+	<span class="glyphicon glyphicon-chevron-right"></span>
+	<span class="sr-only">Next</span>
+</a>
+</div>
+		</div>
+
 	</section>
 
 <section class="col-md-8 col-md-offset " id="s2">
