2a62bc716d044d432554cd6b118b75a93ba76c4f
torofim <torofim@users.noreply.github.com>
2018-03-01 13:44:10 -0700

    casi casi

--- CambiandoDestinos/storage/framework/views/cc7b1111f3a344243758f5acfb20001df8196f32.php
+++ CambiandoDestinos/storage/framework/views/cc7b1111f3a344243758f5acfb20001df8196f32.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 		<link href="./bootstrap.min.css" rel="stylesheet">
+
 	<link rel="stylesheet" type="text/css" href="./css/bootstrap.css">
 	<link rel="stylesheet" type="text/css" href="./css/estilos.css">
 	<link href="./css/main.css" type="text/css" rel="stylesheet">
@@ -42,7 +43,7 @@
 	                  <a href="#nuestros">Cambiando Destinos</a>
 	              </li>
 	              <li class="page-scroll">
-	                  <a href="#informes">Informes</a>
+	                  <a href="#informes">Logros</a>
 	              </li>
 	              <li class="page-scroll">
 	                  <a href="#contact">Contacto</a>
@@ -57,7 +58,7 @@
     <div class="container">
         <div class="rowheader">
             <div class="col-lg-12">
-                <img class="img-responsive" src="./img/elderly_friend.jpg" alt="">
+                <img class="img-responsive" src="./img/libreta1.jpg" alt="">
                 <div class="intro-text">
                     <span class="name">Banksy</span>
                     <hr class="star-light">
@@ -127,7 +128,7 @@
     </section>
 		<section class="success1" id="about">
 				<div class="container3">
-						<div class="col-lg-12 rows">
+						<div class="col-lg-12 rows"  style="background:url(./img/fondochido.jpg) no-repeat center center ; background-size:cover;" >
 								<div class="col-lg-12 text-center">
 										<h2>Cambiando Destinos</h2>
 								</div>
@@ -172,101 +173,101 @@
 						</div>
 				</div>
 		</section>
-		<section class="success1" id="about">
-				<div class="container3">
-						<div class="col-lg-12 rows">
-								<div class="col-lg-12 text-center">
-										<h2>Informes</h2>
-								</div>
-							<div class="col-lg-12 columna">
-								<div class="col-lg-5 informe">
-									<div class="jumbotron">
-									  <p>8 terapias de rehabilitación física al día</p>
-									  <p>3 años y 2 semanas un total de 2,564 terapias físicas rehabilitatorias </p>
-										<p>70% fueron becas gratuitas</p>
-										<p>Este programa implico un gasto aproximado de $ 384,600.00 pesos</p>
-									</div>
-								</div>
-								<div class="col-lg-5 informe">
-								<div class="jumbotron">
-									<p>Visita domiciliaria</p>
-									<p>de octubre del 2011 a finales del 2016  </p>
-									<p>se han logrado 9,112 visitas</p>
 
-								</div>
-							</div>
-							</div>
+		<section class="success1" id="about" >
+			<div class="col-lg-12 text-center">
+					<h2 >Logros</h2>
+			</div>
+				<div class="col-lg-10 container4" >
 
-							<div class="col-lg-12 columna">
-								<div class="col-lg-5 informe">
-									<div class="jumbotron">
-									  <p>Comida caliente brinda en el hogar del beneficiario 1 día a la semana
-											 alimentos preparados adecuadamente de octubre del 2011 a finales del 2016
-											 se han otorgado 3,032 platillos y se realiza en las instalaciones de la institución</p>
+					<div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+			<li data-target="#myCarousel" data-slide-to="3"></li>
+      <li data-target="#myCarousel" data-slide-to="4"></li>
+			<li data-target="#myCarousel" data-slide-to="5"></li>
+    </ol>
 
-									</div>
-								</div>
-								<div class="col-lg-5 informe">
-								<div class="jumbotron">
-									<p>Taller aprendiendo a vivir en la 3era edad</p>
-									<p>De octubre del 2011 a finales del 2016 </p>
-									<p>se impartieron 239 talleres</p>
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" >
 
-								</div>
-							</div>
-							</div>
-							<div class="col-lg-12 columna">
-								<div class="col-lg-5 informe">
-									<div class="col-lg-12 jumbotron">
-										 <p style="text-align:center; font-size:4em;margin-left:-10%; color:black;" >Talleres</p>
-										 <p style="text-align:center;margin-left:-10%;font-size:1.5em">De octubre del 2011 <br>a finales del 2015</p>
-										 <div class=" con" style="margin-left:5%;margin-top:-10%;width:30%;background-color:red;">
-											 <div class="circle-text" style="background:url(./img/palette1.png); background-size:fill"  >
- 												Pintura <br>Artística
- 											</div>
+      <div class="item active">
+        <img src="./img/apoyo.jpg" alt="Los Angeles" style="width:100%;max-height:58em;">
+        <div class="carousel-caption" style="top:20%;">
+          <h3 style="font-size:5em;">GESTIONES </h3>
+          <p>185 pacientes en consulta con especialistas,<br>
+							21 estudios de Tomografía,<br>
+							46 estudios de Laboratorio,<br>
+							7 Cirugías, (Labio leporino, discapacidad infantil)<br>
+							104 entrega de medicamentos,<br>
+							28 préstamos  de aparatos de apoyo funcional (silla de rueda, muletas, bastón, andador, silla para baño, camas tipo hospital, colchones entre otros.)
+							</p>
+        </div>
+      </div>
 
-										 </div>
-										 <p style="margin-left:7%;font-size:1.5em">832 talleres <br> de pintura</p>
-											<div class=" con" style="margin-left:40%; margin-top:-10%;background-color:red;width:30%;">
-												<div class=" circle-text" style="background:url(./img/acustic1.png); background-size:fill" >
-													Música
-													</div>
-													<p style="font-size:1.5em;margin-left:-70%">1248 talleres <br> de música </p>
-											</div>
+      <div class="item">
+        <img src="./img/comida.jpg" alt="Chicago" style="width:100%;max-height:58em;">
+        <div class="carousel-caption" style="top:30%;">
+          <h3 style="font-size:5em;">COMIDA CALIENTE</h3>
+          <p>De octubre del 2011 a finales del 2016
+							se han otorgado 3,032 platillos</p>
+        </div>
+      </div>
+			<div class="item">
+        <img src="./img/rehabilitation.jpg" alt="Chicago" style="width:100%;max-height:58em;">
+        <div class="carousel-caption" style="top:25%;">
+          <h3 style="font-size:5em;">REHABILITACION</h3>
+          <p>8 terapias de rehabilitación física al día <br>
+						3 años y 2 semanas un total de 2,564 terapias físicas rehabilitatorias <br>
+						70% fueron becas gratuitas <br>
+						Este programa implico un gasto aproximado de $ 384,600.00 pesos</p>
+        </div>
+      </div>
+			<div class="item">
+        <img src="./img/visita1.jpg" alt="Chicago" style="width:100%;max-height:58em;">
+        <div class="carousel-caption" style="top:40%;">
+          <h3 style="font-size:5em;">VISITA DOMICILIARIA</h3>
+          <p>De octubre del 2011 a finales del 2016
+							se han logrado 9,112 visitas</p>
+        </div>
+      </div>
 
+      <div class="item">
+        <img src="./img/guitarra.jpg" alt="New York" style="width:100%;max-height:58em;">
+        <div class="carousel-caption"style="top:20%;" >
+          <h3 style="font-size:5em;">INSTITUTO CULTURAL CAMBIANDO DESTINOS </h3>
+          <p>Otorga  gratuitamente talleres de pintura artística y música <br>
+						 Nuestra institución facilita todos los materiales para pintura artística
+						 y los instrumentos para el taller <br>
+						  Estos son impartidos en las instalaciones de nuestra institución <br>
+							De octubre del 2011 al finales del  2015 se impartieron 1248 talleres de música
+							y 832 talleres de pintura beneficiando a un aproximado de 115 alumnos</p>
+        </div>
+      </div>
 
+    </div>
 
-											<div class=" con" style="margin-left:10%;margin-top:-5%;width:30%;background-color:red;">
-											<div class=" circle-text" style="background:url(./img/team.png); ">
-												115 alumnos
-												</div>
-												</div>
-
-								</div>
-								</div>
-
-								<div class="col-lg-5 informe">
-								<div class="jumbotron">
-									<p>GESTIONES HOY POR TI </p>
-									<p>del 2011 al 2017  </p>
-									<p>185 pacientes en consulta con especialistas,
-											21 estudios de resonancia,
-											46 estudios de laboratorio,
-											7 cirugías,
-											104 entrega de medicamentos,
-											28 préstamos de aparatos de apoyo funcional (silla de rueda, muletas, bastón, andador, silla para baño, camas tipo hospital, colchones entre otros.)
-										</p>
-
-								</div>
-							</div>
-							</div>
-
-						</div>
-				</div>
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next" >
+      <span class="glyphicon glyphicon-chevron-right" ></span>
+      <span class="sr-only" >Next</span>
+    </a>
+  </div>
+</div>
 		</section>
+
+
+
+
+
 		<section class="success1" id="about">
-
-
 					<div class="col-lg-12 text-center" style="margin-top:8%;">
 							<h2>Galeria</h2>
 						</div>
@@ -276,53 +277,39 @@
 		<div class="carouselGallery-grid hidden-xs">
         <div class="row">
             <div class="carouselGallery-col-60">
-                <div class="carouselGallery-col-1 carouselGallery-carousel" data-index="0" data-username="visitsweden" data-imagetext="Photographer: @conny_lundstrom
-                If you want to visit Sweden to watch and/or photograph the northern lights, Abisko is a great place to go. Although it's pretty common to see them anywhere in northern Sweden.
+                <div class="carouselGallery-col-1 carouselGallery-carousel" data-index="0" data-username="Taller de Pintura" data-imagetext="
 
-                Tag your photos with #visitsweden and #swedishmoments to get featured. //@deskriptiv" data-location="" data-likes="3144" data-imagepath="./img/pintura.jpg" data-posturl="https://instagram.com/p/9_dViYwVWJ/" style="background-image:url(./img/pintura.jpg);">
+                " data-location="" data-likes="3144" data-imagepath="./img/pintura.jpg" data-posturl="https://instagram.com/p/9_dViYwVWJ/" style="background-image:url(./img/pintura.jpg);">
                 <div class="carouselGallery-item">
 
                 </div>
             </div>
-            <div class="carouselGallery-col-1 carouselGallery-carousel" data-index="1" data-username="visitsweden" data-imagetext="Photographer: @s_gustavsson
-            Location: Gaperud, Värmland
-            Tag your photos with #visitsweden and #swedishmoments to get featured. //@deskriptiv" data-location="" data-likes="5094" data-imagepath="./img/junta.jpg" data-posturl="https://instagram.com/p/96UTFPwVaN/" style="background-image:url(./img/junta.jpg);">
+            <div class="carouselGallery-col-1 carouselGallery-carousel" data-index="1" data-username="En una junta" data-imagetext="
+          " data-location="" data-likes="" data-imagepath="./img/junta.jpg" data-posturl="https://instagram.com/p/96UTFPwVaN/" style="background-image:url(./img/junta.jpg);">
             <div class="carouselGallery-item">
 
             </div>
         </div>
-        <div class="carouselGallery-col-1 carouselGallery-carousel" data-index="2" data-username="visitsweden" data-imagetext="Photographer: @tannerstedtphotography
-        Location: Resmo
-
-        A perfect place for stargazing.
-        Tag #visitsweden and #swedishmoments for a chance to get featured. //@deskriptiv" data-location="" data-likes="3939" data-imagepath="./img/guitarra.jpg" data-posturl="https://instagram.com/p/92tWKsQVUN/" style="background-image:url(./img/guitarra.jpg);">
+        <div class="carouselGallery-col-1 carouselGallery-carousel" data-index="2" data-username="Clases de Guitarra" data-imagetext=""
+         data-location="" data-likes="" data-imagepath="./img/guitarra.jpg" data-posturl="https://instagram.com/p/92tWKsQVUN/" style="background-image:url(./img/guitarra.jpg);">
         <div class="carouselGallery-item">
 
         </div>
     </div>
-    <div class="carouselGallery-col-1 carouselGallery-carousel" data-index="3" data-username="visitsweden" data-imagetext="Photographer: @plilja
-    Location: Kalvträsk
-
-    There are plenty of remote places to rest your soul in northern Sweden.
-
-    Tag your photos with #visitsweden and #swedishmoments to get featured. //@deskriptiv" data-location="" data-likes="4626" data-imagepath="./img/proteccion.jpg" data-posturl="https://instagram.com/p/91p1nGwVVP/" style="background-image:url(./img/proteccion.jpg);">
+    <div class="carouselGallery-col-1 carouselGallery-carousel" data-index="3" data-username="Proteccion al Abuelo" data-imagetext="
+    " data-location="" data-likes="" data-imagepath="./img/proteccion.jpg" data-posturl="https://instagram.com/p/91p1nGwVVP/" style="background-image:url(./img/proteccion.jpg);">
     <div class="carouselGallery-item">
 
     </div>
 </div>
-<div class="carouselGallery-col-1 carouselGallery-carousel" data-index="4" data-username="visitsweden" data-imagetext="Photographer: @jeppe.gustafsson
-Location: Motala
-It won't be long until Sweden turns into a winter wonderland.
-Tag your photos with #visitsweden and #swedishmoments to get featured. //@deskriptiv" data-location="" data-likes="3652" data-imagepath="./img/revision.jpg" data-posturl="https://instagram.com/p/9y6I2YwVb1/" style="background-image:url(./img/revision.jpg);">
+<div class="carouselGallery-col-1 carouselGallery-carousel" data-index="4" data-username="Revisando a Juanito" data-imagetext="
+" data-location="" data-likes="3652" data-imagepath="./img/revision.jpg" data-posturl="https://instagram.com/p/9y6I2YwVb1/" style="background-image:url(./img/revision.jpg);">
 <div class="carouselGallery-item">
 
 </div>
 </div>
-<div class="carouselGallery-col-1 carouselGallery-carousel" data-index="5" data-username="visitsweden" data-imagetext="Photographer: @photobyqase
-Location: Farsta
-There's just something beautifully serene about Swedish autumn.
-
-Remember to tag #visitsweden and #swedishmoments to get featured. //@deskriptiv" data-location="" data-likes="5358" data-imagepath="./img/reunion.jpg" data-posturl="https://instagram.com/p/9tuxNaQVUj/" style="background-image:url(./img/reunion.jpg);">
+<div class="carouselGallery-col-1 carouselGallery-carousel" data-index="5" data-username="Aprendiendo a vivir en la 3ra edad" data-imagetext="
+" data-location="" data-likes="5358" data-imagepath="./img/reunion.jpg" data-posturl="https://instagram.com/p/9tuxNaQVUj/" style="background-image:url(./img/reunion.jpg);">
 <div class="carouselGallery-item">
 
 </div>
@@ -343,7 +330,17 @@ Remember to tag #visitsweden and #swedishmoments to get featured. //@deskriptiv"
 
     </section>
 
-    <footer class="container" style="min-height:200px; background-color:	#000000;color:#fff;text-align:center;padding-top:50px;">
+    <footer class=" container" style=" background-color:	#000000;color:#fff;text-align:center;width:100%;height:40em;">
+			<div class="col-md-12 todo">
+				<div class="col-md-6 izq">
+
+				</div>
+				<div class="col-md-6 der">
+						<h4 style="color:white;font-size:5em;">Contacto</h4>
+				</div>
+
+			</div>
+
 
     </footer>
 </div>
@@ -355,5 +352,6 @@ Remember to tag #visitsweden and #swedishmoments to get featured. //@deskriptiv"
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
 <script src="js/javascript.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </body>
 </html>
